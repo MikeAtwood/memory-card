13142b3b203146e9172b7514a3fafa1153b284ce
Deadpool1 <atwood169@yahoo.com>
2023-10-10 18:06:55 -0500

    setup PokemonList component and css

---
+++ src/components/PokemonList.js
@@ -0,0 +1,14 @@
+import { useEffect } from "react"
+
+const PokemonList = () => {
+    useEffect(() => {
+    })
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PokemonList
