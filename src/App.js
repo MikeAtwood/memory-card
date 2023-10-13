2cdc234cce4c3d0b54009aacb8554ac6babca3b1
Deadpool1 <atwood169@yahoo.com>
2023-10-13 10:24:28 -0500

    fixed useState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import './App.css';
 import { useState } from 'react';
+import Axios from 'axios';
 
 function App() {
 
-  const { pokemonName, setPokemonName } = useState('')
+  const [ pokemonName, setPokemonName ] = useState("")
+
+  const searchPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response)=> {
+      console.log(response)
+    })
+  }
 
   return (
     <div className="App">
       <div className='header-container'>
         <h1>Pokemon Dexter</h1>
-        <input placeholder='Search Pokemon' onChange={(event) => {setPokemonName(event.target.value)}}></input>
-        <button className='submit-search'>Submit</button>
+        <input type='text' 
+          placeholder='Search Pokemon' 
+          onChange={(event) => {
+            setPokemonName(event.target.value)
+          }}
+        />
+        <button onClick={searchPokemon} className='submit-search'>Submit</button>
       </div>
-      
     </div>
   );
 }
