c55d29279ecd73a0f8ad825cf0d7380b233ec17e
Deadpool1 <atwood169@yahoo.com>
2023-10-13 10:45:34 -0500

    updated styling

--- src/App.js
+++ src/App.js
@@ -5,10 +5,29 @@ import Axios from 'axios';
 function App() {
 
   const [ pokemonName, setPokemonName ] = useState("")
+  const [ pokemonChosen, setPokemonChosen ] = useState(false)
+  const [ pokemon, setPokemon ] = useState({
+    name: pokemonName, 
+        species: "", 
+        image: "", 
+        hp: "",
+        attack: "",
+        defense: "",
+        type: "",
+  })
 
   const searchPokemon = () => {
     Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response)=> {
-      console.log(response)
+      setPokemon({
+        name: pokemonName, 
+        species: response.data.species, 
+        image: response.data.sprites.front_default, 
+        hp: response.data.stats[0].base_stat,
+        attack: response.data.stats[1].base_stat,
+        defense: response.data.stats[2].base_stat,
+        type: response.data.types[0].type.name,
+      })
+      setPokemonChosen(true)
     })
   }
 
@@ -23,6 +42,9 @@ function App() {
           }}
         />
         <button onClick={searchPokemon} className='submit-search'>Submit</button>
+      </div>
+      <div className='DisplaySection'>
+          {!pokemonChosen ? (<h1> Please choose a Pokemon</h1>) : (<h1>{pokemonName}</h1>)}
       </div>
     </div>
   );
