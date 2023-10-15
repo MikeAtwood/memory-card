0a2e449edca31011734b9c3704b5aa8a76ab416f
Deadpool1 <atwood169@yahoo.com>
2023-10-15 11:22:32 -0500

    updated PokemonList api fetch request

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -8,7 +8,19 @@ const PokemonList = () => {
         const fetchPokemonList = async () => {
             try {
                 const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=100")
-                setPokemonDex(response.data.results)
+                const pokemonList = response.data.results;
+
+                const pokemonDetails = await Promise.all(
+                    pokemonList.map(async (pokemon) => {
+                        const res = await axios.get(pokemon.url)
+                        return {
+                            name: pokemon.name,
+                            id: res.data.id,
+                            sprite: res.data.sprites.front_default,
+                        }
+                    })
+                )
+                setPokemonDex(pokemonDetails)
             } catch (error) {
                 console.log("Error fetching Pokemon List")
             }
@@ -22,7 +34,7 @@ const PokemonList = () => {
             <div className="pokedex-container">
                 {pokemonDex.map((pokemon) => (
                     <div key={pokemon.name} className="pokemon-box">
-                        <img src={`https://pokeapi.co/media/sprites/pokemon/${pokemon.id}.png`} alt={pokemon.name} />
+                        <img src={pokemon.sprite} alt={pokemon.name} />
                         <p>{pokemon.name}</p>
                     </div>
                 ))}
